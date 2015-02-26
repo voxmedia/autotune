3273fada5bcf3b1ba5fa4030710d0d01ece1130c
Ryan Mark <ryan@mrk.cc>
2015-02-26 11:23:46 -0500

    Ported over JS frontend from gob prototype closes #7

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    browserify: {
+      dist: {
+        files: {
+          'public/app.js': ['lib/js/app.js']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: 'public/app.js',
+        dest: 'public/app.min.js'
+      },
+    },
+    nodeunit: {
+      files: ['test/js/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        options: {
+          jshintrc: 'lib/js/.jshintrc'
+        },
+        src: ['lib/js/**/*.js']
+      },
+      test: {
+        src: ['test/js/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'browserify']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'browserify', 'nodeunit', 'uglify']);
+
+};
