d5911bfcf0f080d5f12d68920abc88f1209791da
Ryan Mark <ryan@mrk.cc>
2015-03-16 10:58:18 -0400

    stash git url regex for later, fix nav bar link

--- lib/autotune.rb
+++ lib/autotune.rb
@@ -6,4 +6,6 @@ module Autotune
   SLUG_OR_ID_REGEX = /([-\w]+|\d+)/
   # regex for matching auth http header
   AUTH_KEY_RE = /API-KEY\s+auth="?([^"]+)"?/
+  # regex for verifying clonable git urls
+  REPO_URL_RE = %r{(\w+://)?(.+@)?([\w\.]+)(:[\d]+)?/?(.*)}
 end
