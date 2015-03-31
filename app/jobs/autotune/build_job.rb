ca4e1261db737d62ce60ac7efd97edc0aa925778
Ryan Mark <ryan@mrk.cc>
2015-03-31 10:45:26 -0400

    added base_url field to build data

--- app/jobs/autotune/build_job.rb
+++ app/jobs/autotune/build_job.rb
@@ -13,6 +13,9 @@ module Autotune
       # Setup the snapshot if it's not already. We don't want to update
       # our snapshot, we just need it to exist
       SyncProjectJob.perform_now(project) unless snapshot.exist?
+      # Add a few extras to the build data
+      build_data = project.data.dup
+      build_data['base_url'] = project.preview_url
       # Run the build
       out = snapshot.build(project.data)
       # Upload build
