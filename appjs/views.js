1a32a916b0d6239102a4cc0578afc41b6865766e
Ryan Mark <ryan@mrk.cc>
2015-04-30 15:57:38 -0400

    fix blueprint chooser, closes #113

--- appjs/views.js
+++ appjs/views.js
@@ -4,7 +4,7 @@ module.exports = {
   ListBlueprints: require('./views/ListBlueprints'),
   EditBlueprint: require('./views/EditBlueprint'),
   ShowBlueprint: require('./views/ShowBlueprint'),
-  ChooseBlueprint: require('./views/ShowBlueprint'),
+  ChooseBlueprint: require('./views/ChooseBlueprint'),
   BlueprintBuilder: require('./views/BlueprintBuilder'),
   ListProjects: require('./views/ListProjects'),
   EditProject: require('./views/EditProject'),
