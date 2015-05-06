28e68384a2dd0fe75a71e2b764d4b181e22fed8f
Ryan Mark <ryan@mrk.cc>
2015-05-06 15:19:16 -0400

    stupid mysql and rails

--- db/migrate/20150506174111_add_user_to_projects.rb
+++ db/migrate/20150506174111_add_user_to_projects.rb
@@ -1,6 +1,6 @@
 class AddUserToProjects < ActiveRecord::Migration
   def change
-    add_reference :autotune_projects, :user, index: true, foreign_key: true
-    add_foreign_key :autotune_projects, :autotune_user, column: :user_id
+    add_reference :autotune_projects, :user, index: true
+    add_foreign_key :autotune_projects, :autotune_users, column: :user_id
   end
 end
