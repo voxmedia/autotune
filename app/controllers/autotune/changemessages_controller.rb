46e8b4a03e1ecd1bfb84b81cd14d317e372a9c7b
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-05-08 11:08:54 -0400

    Adding a SSE streaming controller for #82

---
+++ app/controllers/autotune/changemessages_controller.rb
@@ -0,0 +1,22 @@
+require_dependency 'autotune/application_controller'
+require 'redis'
+
+module Autotune
+  class ChangemessagesController < ApplicationController
+    include ActionController::Live
+
+    def index
+      response.headers['Content-Type'] = 'text/event-stream'
+      sse = SSE.new(response.stream, retry: 300, event: "change")
+       sse.write({msg: 'Channel init'})
+
+       $redis.subscribe('blueprints', 'projects') do |on|
+          on.message do |channel, msg|
+            sse.write({msg: msg})
+          end
+        end
+    ensure
+      sse.close
+    end
+  end
+end
