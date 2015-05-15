5d0aaac7a9acefde91f85e2c20ff5c821a38e204
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-05-15 11:47:09 -0400

    Move heartbeat from global to per sse connection

--- app/controllers/autotune/changemessages_controller.rb
+++ app/controllers/autotune/changemessages_controller.rb
@@ -10,30 +10,35 @@ module Autotune
     def index
       response.headers['Content-Type'] = 'text/event-stream'
       sse = SSE.new(response.stream, retry: 300, event: 'connectionopen')
-      sse.write(msg: 'Channel init')
-      stream_events(sse, Time.zone.now)
+      sse.write(msg: 'Channel init')      
+      heartbeat(sse)
+      stream_events(sse)
     ensure
       sse.close
     end
 
     private
 
-    def stream_events(sse, start_time)
-      $redis.subscribe('blueprints', 'projects', 'heartbeat') do |on|
-        on.message do |channel, msg|
-          if (channel == 'heartbeat')
-            sse.write({ msg: msg }, retry: 3000, event: 'heartbeat')
-          else
-            sse.write({ msg: msg }, event: 'change')
-          end
-
-          # Close connection before server can kill the thread
-          time_alive = Time.zone.now - start_time
-          if (time_alive >= KEEPALIVE_SEC)
-            sse.write({ msg: 'Channel close', timeAlive: time_alive }, event: 'connectionclose')
-            sse.close
-          end
+    def stream_events(sse)
+      $redis.subscribe('blueprints', 'projects') do |on|
+        on.message do |_channel, msg|
+          sse.write({ msg: msg }, event: 'change')
         end
+      end
+    end
+
+    def heartbeat(sse)
+      Thread.new do
+        counter = 1
+        while counter <= KEEPALIVE_SEC
+          sse.write({ msg: counter }, retry: 3000, event: 'heartbeats')
+          counter += 1
+          sleep 1
+        end
+          sse.write({ msg: 'Channel close'}, event: 'connectionclose')
+          sse.close
+          Thread.exit
+
       end
     end
   end
