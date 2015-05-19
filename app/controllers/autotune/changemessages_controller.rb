5dc1999f30991cded5cd32671a1026d6f75b2140
Ryan Mark <ryan@mrk.cc>
2015-05-19 16:42:03 -0400

    move around some streaming things, hopefully helps #11

--- app/controllers/autotune/changemessages_controller.rb
+++ app/controllers/autotune/changemessages_controller.rb
@@ -5,45 +5,50 @@ module Autotune
   class ChangemessagesController < ApplicationController
     include ActionController::Live
 
-    KEEPALIVE_SEC = 60 # Make sure that this is lower than the server's timeout
+    before_action :close_db_connection
+
+    TIMEOUT = 60
 
     def index
+      t1 = Time.now.to_f
+      logger.info 'Client stream connected'
       response.headers['Content-Type'] = 'text/event-stream'
       sse = SSE.new(response.stream, retry: 300, event: 'connectionopen')
       sse.write(msg: 'Channel init')
-      # heartbeat(sse)
-      stream_events(sse)
+
+      redis_thread = Thread.new do
+        Thread.current.abort_on_exception = true
+        Autotune.redis.subscribe('blueprints', 'projects') do |on|
+          on.message do |_channel, msg|
+            sse.write({ msg: msg }, event: 'change')
+          end
+        end
+      end
+
+      loop do
+        if Time.now.to_f - t1 > TIMEOUT
+          logger.info 'Preemptive stream timeout'
+          sse.write(msg: 'Channel close', event: 'connectionclose')
+          break
+        end
+        sse.write(msg: 'pong', event: 'ping')
+        sleep 2
+      end
+    rescue ClientDisconnected
+      logger.info 'Client stream disconnected'
     ensure
       sse.close
+      if redis_thread.alive?
+        logger.info 'Teardown redis thread'
+        redis_thread.exit
+      end
+      logger.info 'Cleaned up stream threads'
     end
 
     private
 
-    def stream_events(sse)
-      Autotune.redis.subscribe('blueprints', 'projects') do |on|
-        on.message do |_channel, msg|
-          sse.write({ msg: msg }, event: 'change')
-        end
-      end
-    end
-
-    def heartbeat(sse)
-      Thread.new do
-        counter = 1
-        while counter <= KEEPALIVE_SEC
-          begin
-            sse.write({ msg: counter }, retry: 3000, event: 'heartbeats')
-            counter += 1
-            sleep 1
-          rescue IOError
-            sse.close
-            Thread.exit
-          end
-        end
-        sse.write({ msg: 'Channel close' }, event: 'connectionclose')
-        sse.close
-        Thread.exit
-      end
+    def close_db_connection
+      ActiveRecord::Base.connection_pool.release_connection
     end
   end
 end
