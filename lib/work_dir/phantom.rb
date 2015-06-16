cfda4a3909d928fa0d5c8bfd9c0abac3b58c9755
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-06-16 14:05:26 -0400

    Change phantom to use the screenhot js

--- lib/work_dir/phantom.rb
+++ lib/work_dir/phantom.rb
@@ -5,45 +5,9 @@ module WorkDir
   class Phantom < Base
     def capture_screenshot(url)
       working_dir do
-        cmd 'echo "var WebPage = require(\'webpage\');
-            var System = require(\'system\');
-            address = System.args[1];
-            page = WebPage.create();
-            page.viewportSize = {
-              width: 975,
-              height: 500
-            };
-            page.open(address);
-            page.onLoadFinished = function() {
-               setTimeout(function() {
-                page.render(\'screenshotL.png\');
-                page.close();
-            }, 2000);}
-
-            page2 = WebPage.create();
-            page2.viewportSize = {
-              width: 720,
-              height: 400
-            };
-            page2.open(address);
-            page2.onLoadFinished = function() {
-               setTimeout(function() {
-                page2.render(\'screenshotM.png\');
-            }, 2000);}
-            
-            page3 = WebPage.create();
-            page3.viewportSize = {
-              width: 400,
-              height: 200
-            };
-            page3.open(address);
-            page3.onLoadFinished = function() {
-               setTimeout(function() {
-                page3.render(\'screenshotS.png\');
-                phantom.exit();
-            }, 2000);}" > screenshot.js'
+        cp '../../../../autotune/lib/work_dir/screenshot.js', '.'
         phantomjs 'screenshot.js', url
-        # rm 'screenshot.js'
+        rm 'screenshot.js'
       end
     end
 
