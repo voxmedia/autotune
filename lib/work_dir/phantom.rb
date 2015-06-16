69047e93289351223ed2ed7d10182130fbba6050
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-06-16 11:47:28 -0400

    A lo-fi way to take screenshots at multiple sizes

--- lib/work_dir/phantom.rb
+++ lib/work_dir/phantom.rb
@@ -6,21 +6,44 @@ module WorkDir
     def capture_screenshot(url)
       working_dir do
         cmd 'echo "var WebPage = require(\'webpage\');
-        var System = require(\'system\');
-        address = System.args[1];
-        page = WebPage.create();
-        page.viewportSize = {
-          width: 800,
-          height: 400
-        };
-        page.open(address);
-        page.onLoadFinished = function() {
-           setTimeout(function() {
-            page.render(\'screenshot.png\');
-            phantom.exit();
-        }, 1000);}" > screenshot.js'
+            var System = require(\'system\');
+            address = System.args[1];
+            page = WebPage.create();
+            page.viewportSize = {
+              width: 975,
+              height: 500
+            };
+            page.open(address);
+            page.onLoadFinished = function() {
+               setTimeout(function() {
+                page.render(\'screenshotL.png\');
+                page.close();
+            }, 2000);}
+
+            page2 = WebPage.create();
+            page2.viewportSize = {
+              width: 720,
+              height: 400
+            };
+            page2.open(address);
+            page2.onLoadFinished = function() {
+               setTimeout(function() {
+                page2.render(\'screenshotM.png\');
+            }, 2000);}
+            
+            page3 = WebPage.create();
+            page3.viewportSize = {
+              width: 400,
+              height: 200
+            };
+            page3.open(address);
+            page3.onLoadFinished = function() {
+               setTimeout(function() {
+                page3.render(\'screenshotS.png\');
+                phantom.exit();
+            }, 2000);}" > screenshot.js'
         phantomjs 'screenshot.js', url
-        rm 'phantomjs'
+        # rm 'screenshot.js'
       end
     end
 
