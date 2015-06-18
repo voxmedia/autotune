c6c52e8c12257d12b34ce8e684a6a4c9e5da1b4a
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-06-18 14:35:16 -0400

    temporarily adding screenshot js inline to test on staging

--- lib/work_dir/phantom.rb
+++ lib/work_dir/phantom.rb
@@ -6,7 +6,54 @@ module WorkDir
     def capture_screenshot(url)
       return unless phantomJS?
       working_dir do
-        cp '../../../../autotune/lib/work_dir/screenshot.js', 'screenshot.js'
+        cmd 'echo "var WebPage = require(\'webpage\'),
+              System = require(\'system\'),
+              address = System.args[1],
+              index = 0;
+
+          // All the sizes to screenshot. 
+          // Note: PhantomJs uses the heights specified here as a min-height criteria
+          var screenshots = [
+            {\'dimensions\' : [970,300],
+            \'filename\': \'./screenshots/screenshot_l.png\'},
+            {\'dimensions\' : [720,300],
+            \'filename\': \'./screenshots/screenshot_m.png\'},
+            {\'dimensions\' : [400,200],
+            \'filename\': \'./screenshots/screenshot_s.png\'}
+          ];
+
+          var capturescreen = function(){
+            var page = WebPage.create();
+            page.viewportSize = {
+              width: screenshots[index].dimensions[0],
+              height: screenshots[index].dimensions[1]
+            };
+            page.open(address);
+
+            page.onError = function(){
+              page.close();
+              setTimeout(nextPage, 1000);
+            }
+
+            page.onLoadFinished = setTimeout(function() {
+                page.render(screenshots[index].filename);
+                page.close();
+                index++;
+                // Give it a second before calling next. 
+                // Phantom runs into some sort of race condition without this
+                setTimeout(nextPage, 1000); 
+            }, 3000);
+          }
+
+          var nextPage = function(){
+            if(!screenshots[index]){
+              phantom.exit();
+            }
+            capturescreen();
+          }
+
+          nextPage();" > screenshot.js'
+          
         phantomjs 'screenshot.js', url
         rm 'screenshot.js'
       end
