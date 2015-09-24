0dee83ded391bfef45a209fa5188ee4cd4fa5b32
Ryan Mark <ryan@mrk.cc>
2015-09-24 10:23:19 -0400

    revert editproject changes, fixes missing buttons

--- appjs/views/EditProject.js
+++ appjs/views/EditProject.js
@@ -109,158 +109,173 @@ var EditProject = BaseView.extend(require('./mixins/actions'), require('./mixins
 
   renderForm: function(resolve, reject) {
     var $form = this.$('#projectForm'),
-        buttonTmpl = require('../templates/project_buttons.ejs'),
-        formConfig, newProject;
+        button_tmpl = require('../templates/project_buttons.ejs'),
+        form_config, config_themes, newProject;
 
     // Prevent return or enter from submitting the form
     $form.keypress(function(event){
-      var fieldType = event.originalEvent.srcElement.type;
+      var field_type = event.originalEvent.srcElement.type;
       if (event.keyCode === 10 || event.keyCode === 13){
-        if(fieldType !== 'textarea'){
+        if(field_type !== 'textarea'){
           event.preventDefault();
         }
       }
     });
 
-    formConfig = JSON.parse( JSON.stringify( this.model.getFormConfig() ) );
-    newProject = this.model.isNew() || this.copyProject;
+    if ( this.model.isNew() && !this.copyProject ) {
+      newProject = true;
+      form_config = this.model.blueprint.get('config').form;
+      config_themes = this.model.blueprint.get('config').themes || ['generic'];
+    } else if (this.copyProject) {
+      newProject = true;
+      form_config = this.model.get('blueprint_config').form;
+      config_themes = this.model.get('blueprint_config').themes || ['generic'];
+    } else {
+      newProject = false;
+      form_config = this.model.get('blueprint_config').form;
+      config_themes = this.model.get('blueprint_config').themes || ['generic'];
+    }
 
-    var configThemes = this.model.getThemes(),
-        themes = this.app.themes.filter(function(theme) {
-          if ( _.isEqual(configThemes, ['generic']) ) {
-            return true;
-          } else {
-            return _.contains(configThemes, theme.get('value'));
-          }
-        }),
-        socialChars = {
-          "sbnation": 8,
-          "theverge": 5,
-          "polygon": 7,
-          "racked": 6,
-          "eater": 5,
-          "vox": 9,
-          "custom": 0
-        },
-        schemaProperties = {
-          "title": {
-            "title": "Title",
-            "type": "string",
-            "required": true
+    if(_.isUndefined(form_config)) {
+      this.app.view.error('This blueprint does not have a form!');
+      reject('This blueprint does not have a form!');
+    } else {
+      var themes = this.app.themes.filter(function(theme) {
+            if ( _.isEqual(config_themes, ['generic']) ) {
+              return true;
+            } else {
+              return _.contains(config_themes, theme.get('value'));
+            }
+          }),
+          social_chars = {
+            "sbnation": 8,
+            "theverge": 5,
+            "polygon": 7,
+            "racked": 6,
+            "eater": 5,
+            "vox": 9,
+            "custom": 0
           },
-          "theme": {
-            "title": "Theme",
-            "type": "string",
-            "required": true,
-            "default": pluckAttr(themes, 'value')[0],
-            "enum": pluckAttr(themes, 'value')
-          },
-          "slug": {
-            "title": "Slug",
-            "type": "string"
-          },
-          "tweet_text":{
-            "type": "string",
-            "minLength": 0
-          }
-        },
-        optionsForm = {
-          "attributes": {
-            "data-model": "Project",
-            "data-model-id": this.model.isNew() ? '' : this.model.id,
-            "data-action": this.model.isNew() ? 'new' : 'edit',
-            "data-next": 'show'
-          }
-        },
-        optionsFields = {
-          "theme": {
-            "type": "select",
-            "optionLabels": pluckAttr(themes, 'label'),
-          },
-          "slug": {
-            "label": "Slug",
-            "validator": function(callback){
-              var slugPattern = /^[0-9a-z\-_]{0,60}$/;
-              var slug = this.getValue();
-              if ( slugPattern.test(slug) ){
-                callback({ "status": true });
-              } else if (slugPattern.test(slug.substring(0,60))){
-                this.setValue(slug.substr(0,60));
-                callback({ "status": true });
-              } else {
-                callback({
-                  "status": false,
-                  "message": "Must contain fewer than 60 numbers, lowercase letters, hyphens, and underscores."
-                });
-              }
+          schema_properties = {
+            "title": {
+              "title": "Title",
+              "type": "string",
+              "required": true
+            },
+            "theme": {
+              "title": "Theme",
+              "type": "string",
+              "required": true,
+              "default": pluckAttr(themes, 'value')[0],
+              "enum": pluckAttr(themes, 'value')
+            },
+            "slug": {
+              "title": "Slug",
+              "type": "string"
+            },
+            "tweet_text":{
+              "type": "string",
+              "minLength": 0
             }
           },
-          "tweet_text":{
-            "label": "Social share text",
-            "constrainMaxLength": true,
-            "constrainMinLength": true,
-            "showMaxLengthIndicator": true
-          }
-        };
+          options_form = {
+            "attributes": {
+              "data-model": "Project",
+              "data-model-id": this.model.isNew() ? '' : this.model.id,
+              "data-action": this.model.isNew() ? 'new' : 'edit',
+              "data-next": 'show'
+            }
+          },
+          options_fields = {
+            "theme": {
+              "type": "select",
+              "optionLabels": pluckAttr(themes, 'label'),
+            },
+            "slug": {
+              "label": "Slug",
+              "validator": function(callback){
+                var slugPattern = /^[0-9a-z\-_]{0,60}$/;
+                var slug = this.getValue();
+                if ( slugPattern.test(slug) ){
+                  callback({ "status": true });
+                } else if (slugPattern.test(slug.substring(0,60))){
+                  this.setValue(slug.substr(0,60));
+                  callback({ "status": true });
+                } else {
+                  callback({
+                    "status": false,
+                    "message": "Must contain fewer than 60 numbers, lowercase letters, hyphens, and underscores."
+                  });
+                }
+              }
+            },
+            "tweet_text":{
+              "label": "Social share text",
+              "constrainMaxLength": true,
+              "constrainMinLength": true,
+              "showMaxLengthIndicator": true
+            }
+          };
 
-    // if there is only one theme option, hide the dropdown
-    if ( themes.length === 1 ) {
-      optionsFields['theme']['type'] = 'hidden';
-    }
-
-    _.extend(schemaProperties, formConfig['schema']['properties'] || {});
-    if( formConfig['options'] ) {
-      _.extend(optionsForm, formConfig['options']['form'] || {});
-      _.extend(optionsFields, formConfig['options']['fields'] || {});
-    }
-
-    var opts = {
-      "schema": {
-        "title": this.model.blueprint.get('title'),
-        "description": this.model.blueprint.get('config').description,
-        "type": "object",
-        "properties": schemaProperties
-      },
-      "options": {
-        "form": optionsForm,
-        "fields": optionsFields,
-        "focus": this.firstrender
-      },
-      "postrender": _.bind(function(control) {
-        this.alpaca = control;
-
-        var theme = control.childrenbypropertyid["theme"],
-           social = control.childrenbypropertyid["tweet_text"];
-
-        social.schema.maxlength = 140 - (26 + socialChars[theme.getvalue()]);
-        social.updatemaxlengthindicator();
-
-        $(theme).on('change', function(){
-          social.schema.maxLength = 140 - (26 + socialChars[theme.getValue()]);
-          social.updateMaxLengthIndicator();
-        });
-
-        this.alpaca.childrenByPropertyId["slug"].setValue(
-          this.model.get('slug_sans_theme') );
-
-        control.form.form.append(
-          helpers.render(buttonTmpl, this.templateData()) );
-
-        resolve();
-      }, this)
-    };
-
-    if( formConfig['view'] ) {
-      opts.view = formConfig.view;
-    }
-
-    if(!this.model.isNew() || this.copyProject) {
-      opts.data = this.model.formData();
-      if ( !_.contains(pluckAttr(themes, 'value'), opts.data.theme) ) {
-        opts.data.theme = pluckAttr(themes, 'value')[0];
+      // if there is only one theme option, hide the dropdown
+      if ( themes.length === 1 ) {
+        options_fields['theme']['type'] = 'hidden';
       }
+
+      _.extend(schema_properties, form_config['schema']['properties'] || {});
+      if( form_config['options'] ) {
+        _.extend(options_form, form_config['options']['form'] || {});
+        _.extend(options_fields, form_config['options']['fields'] || {});
+      }
+
+      var opts = {
+        "schema": {
+          "title": this.model.blueprint.get('title'),
+          "description": this.model.blueprint.get('config').description,
+          "type": "object",
+          "properties": schema_properties
+        },
+        "options": {
+          "form": options_form,
+          "fields": options_fields,
+          "focus": this.firstRender
+        },
+        "postRender": _.bind(function(control) {
+          this.alpaca = control;
+
+          var theme = control.childrenByPropertyId["theme"],
+             social = control.childrenByPropertyId["tweet_text"];
+
+          social.schema.maxLength = 140-(26+social_chars[theme.getValue()]);
+          social.updateMaxLengthIndicator();
+
+          $(theme).on('change', function(){
+            social.schema.maxLength = 140-(26+social_chars[theme.getValue()]);
+            social.updateMaxLengthIndicator();
+          });
+
+          this.alpaca.childrenByPropertyId["slug"].setValue(
+            this.model.get('slug_sans_theme') );
+
+          control.form.form.append(
+            helpers.render(button_tmpl, this.templateData()) );
+
+          resolve();
+        }, this)
+      };
+
+      if( form_config['view'] ) {
+        opts.view = form_config.view;
+      }
+
+      if(!this.model.isNew() || this.copyProject) {
+        opts.data = this.model.formData();
+        if ( !_.contains(pluckAttr(themes, 'value'), opts.data.theme) ) {
+          opts.data.theme = pluckAttr(themes, 'value')[0];
+        }
+      }
+      $form.alpaca(opts);
     }
-    $form.alpaca(opts);
   },
 
   formValues: function($form) {
