a55b562c7809e666aeb0cb6b257a26b7712a7585
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-12-21 13:50:25 -0500

    correct referencess. #298 #293

--- db/migrate/20151221172822_create_autotune_groups.rb
+++ db/migrate/20151221172822_create_autotune_groups.rb
@@ -3,5 +3,13 @@ class CreateAutotuneGroups < ActiveRecord::Migration
     create_table :autotune_groups do |t|
       t.string :title
     end
+
+    # Add many to many relation between groups and blueprints
+    create_table :autotune_blueprints_groups, :id => false do |t|
+      t.references :blueprint, index: true
+      t.references :group, index: true
+    end
+    add_foreign_key :autotune_blueprints_groups, :autotune_groups, column: :group_id
+    add_foreign_key :autotune_blueprints_groups, :autotune_blueprints, column: :blueprint_id
   end
 end
--- db/migrate/20151221173210_add_details_to_themes.rb
+++ db/migrate/20151221173210_add_details_to_themes.rb
@@ -1,6 +1,7 @@
 class AddDetailsToThemes < ActiveRecord::Migration
   def change
-    add_column :themes, :meta, :text
-    add_reference :themes, :group, index: true, foreign_key: true
+    add_column :autotune_themes, :theme_config, :text
+    add_column :autotune_themes, :group_id, :integer
+    add_foreign_key :autotune_themes, :autotune_groups, column: :group_id
   end
 end
