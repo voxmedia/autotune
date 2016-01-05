dbf46bb98f19164ad4705e66ba54c206e6ba3ada
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-01-05 15:43:10 -0500

    sort themes by name by default

--- app/models/autotune/theme.rb
+++ app/models/autotune/theme.rb
@@ -2,10 +2,12 @@ module Autotune
   # Themes for blueprints
   class Theme < ActiveRecord::Base
     belongs_to :group
-    
+
     validates :value, :label, :group, :presence => true
     validates :value,
               :uniqueness => true,
               :format => { :with => /\A[0-9a-z\-_]+\z/ }
+
+    default_scope { order('label ASC') }
   end
 end
