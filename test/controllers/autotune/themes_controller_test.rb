e16c980d224e191159eb732c0fd22414b44dd4d7
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-01-07 15:53:10 -0500

    test for themes controller

---
+++ test/controllers/autotune/themes_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+module Autotune
+  class ThemesControllerTest < ActionController::TestCase
+    setup do
+      @routes = Engine.routes
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+    end
+
+  end
+end
