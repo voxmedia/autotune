c836deef1d3524aa6a06cec890e1b627ea02cc97
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-01-25 22:11:10 -0500

    add validations to group

--- app/models/autotune/group.rb
+++ app/models/autotune/group.rb
@@ -1,10 +1,16 @@
 module Autotune
   class Group < ActiveRecord::Base
+    include Slugged
     has_many :themes
-    has_and_belongs_to_many :blueprints
+    has_and_belongs_to_many :projects
     has_many :group_memberships
     has_many :users, through: :group_memberships
 
-    # TODO (Kavya) add validations
+    validates :slug, :name, :presence => true
+    validates :name,
+              :uniqueness => true
+    validates :slug,
+              :uniqueness => true,
+              :format => { :with => /\A[0-9a-z\-_]+\z/ }
   end
 end
