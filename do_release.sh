eb1a7d5a8a4c33bd79080ff5b56069f4cd6b2fa0
Ryan Mark <ryan@mrk.cc>
2016-03-17 12:17:22 -0400

    update release script

--- do_release.sh
+++ do_release.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo $0: usage: do_release.sh version
-    exit 1
+if ! git diff-index --quiet HEAD --; then
+  echo You must commit all your changes before updating the version
+  exit 1
 fi
+
+old_version=$(jq '.version' package.json | tr -d '"')
+
+if [ $# -ne 1 ]; then
+  read -p "Current version is $old_version. Enter a new version: " version
+else
+  version=$1
+fi
+
+if [ "$old_version" = "$version" ]; then
+  echo Already at version $version
+  exit 1
+fi
+
+echo Updating version to $version
 
 command -v jq >/dev/null 2>&1 || { echo >&2 "Missing jq. Please install jq."; exit 1; }
-command -v sponge >/dev/null 2>&1 || { echo >&2 "Missing sponge. Please install moreutils."; exit 1; }
 
-if git diff-index --quiet HEAD --; then
-    # no changes
-  jq ".version |= $1" package.json |sponge package.json
-  git commit -am "bump to $1"
-  git tag $1
-  git push --tags
-else
-  echo You must commit all your changes before updating the version
-    # changes
+{ rm package.json && jq --arg version $version '.version |= $version' > package.json; } < package.json
+
+read -p "Do you wish to commit the new version, tag and push? [y/N] " yn
+if echo "$yn" | grep -iq "^y"; then
+  git commit -am "bump to $version" && git tag v$version && git push --tags
 fi
-
