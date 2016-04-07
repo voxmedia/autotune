49d1ff3cfb1e4fd25514089c4bc8684df1467db4
Ryan Mark <ryan@mrk.cc>
2016-04-07 17:27:49 -0400

    fix sentry error

--- app/controllers/autotune/projects_controller.rb
+++ app/controllers/autotune/projects_controller.rb
@@ -198,7 +198,7 @@ module Autotune
       deployer.before_build(@build_data, {}, current_user)
       render :json => @build_data
     rescue => exc
-      if @project.meta['error_message'].present?
+      if @project.present? && @project.meta['error_message'].present?
         render_error @project.meta['error_message'], :bad_request
       elsif exc.is_a?(Signet::AuthorizationError)
         render_error 'There was an error authenticating your Google account', :bad_request
